454a6ea34f3f2300700e5d991922fc2ae517f46e
Bill <66212885+Bill892@users.noreply.github.com>
2020-12-22 21:52:45 +0200

    Create 01.cs

---
+++ 01.cs
@@ -0,0 +1,11 @@
+string a = "it costs money";
+string b = "i do not have money";
+string c = "im not directly harming people by doing it";
+if (a && b && c)
+{
+    pirate(content);
+}
+if (!a && b && c)
+{
+    Environment.Exit(0);
+}
